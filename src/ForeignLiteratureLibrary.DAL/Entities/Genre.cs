8f99ac1bc222dc273f39644b19aabbc2caae565b
Danilo Chaku <danilochaku@gmail.com>
2024-10-10 19:06:16 +0300

    reafactor: adjust nullability for some entitie's properties

--- src/ForeignLiteratureLibrary.DAL/Entities/Genre.cs
+++ src/ForeignLiteratureLibrary.DAL/Entities/Genre.cs
@@ -5,5 +5,5 @@ public class Genre
     public int GenreID { get; set; }
     public string Name { get; set; } = string.Empty;
 
-    public ICollection<Book>? Books { get; set; }
+    public ICollection<Book> Books { get; set; } = [];
 }
--- src/ForeignLiteratureLibrary.DAL/Entities/Reader.cs
+++ src/ForeignLiteratureLibrary.DAL/Entities/Reader.cs
@@ -5,8 +5,8 @@ public class Reader
     public string LibraryCardNumber { get; set; } = string.Empty;
     public string FullName { get; set; } = string.Empty;
     public DateTime DateOfBirth { get; set; }
-    public string Email { get; set; } = string.Empty;
-    public string Phone { get; set; } = string.Empty;
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
     public DateTime RegistrationDate { get; set; }
 
     public ICollection<BookEditionLoan> Loans { get; set; } = [];
