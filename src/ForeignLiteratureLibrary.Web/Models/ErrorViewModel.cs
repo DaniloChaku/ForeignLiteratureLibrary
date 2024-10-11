89927f001057712d7d9f965e22c2d6ceb486e3a5
Danilo Chaku <danilochaku@gmail.com>
2024-10-11 14:07:53 +0300

    build: replace .API project with .Web (ASP.NET Core MVC)

---
+++ src/ForeignLiteratureLibrary.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ForeignLiteratureLibrary.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
