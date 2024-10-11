98eba41a38939e793e3c5654e2487d84e8da3643
Danilo Chaku <danilochaku@gmail.com>
2024-10-11 19:36:16 +0300

    feat: inject dependencies

--- src/ForeignLiteratureLibrary.Web/Program.cs
+++ src/ForeignLiteratureLibrary.Web/Program.cs
@@ -1,7 +1,36 @@
+using ForeignLiteratureLibrary.BLL.Interfaces;
+using ForeignLiteratureLibrary.BLL.Services;
+using ForeignLiteratureLibrary.DAL.Interfaces;
+using ForeignLiteratureLibrary.DAL.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("Default")!;
+
+builder.Services.AddScoped<IAuthorRepository>(provider => new AuthorRepository(connectionString))
+    .AddScoped<IBookEditionLoanRepository>(provider => new BookEditionLoanRepository(connectionString))
+    .AddScoped<IBookEditionRepository>(provider => new BookEditionRepository(connectionString))
+    .AddScoped<IBookRepository>(provider => new BookRepository(connectionString))
+    .AddScoped<ICountryRepository>(provider => new CountryRepository(connectionString))
+    .AddScoped<IGenreRepository>(provider => new GenreRepository(connectionString))
+    .AddScoped<ILanguageRepository>(provider => new LanguageRepository(connectionString))
+    .AddScoped<IPublisherRepository>(provider => new PublisherRepository(connectionString))
+    .AddScoped<IReaderRepository>(provider => new ReaderRepository(connectionString))
+    .AddScoped<ITranslatorRepository>(provider => new TranslatorRepository(connectionString));
+
+builder.Services.AddScoped<IAuthorService, AuthorService>()
+    .AddScoped<IBookEditionLoanService, BookEditionLoanService>()
+    .AddScoped<IBookEditionService, BookEditionService>()
+    .AddScoped<IBookService, BookService>()
+    .AddScoped<ICountryService, CountryService>()
+    .AddScoped<IGenreService, GenreService>()
+    .AddScoped<ILanguageService, LanguageService>()
+    .AddScoped<IPublisherService, PublisherService>()
+    .AddScoped<IReaderService, ReaderService>()
+    .AddScoped<ITranslatorService, TranslatorService>();
 
 var app = builder.Build();
 
